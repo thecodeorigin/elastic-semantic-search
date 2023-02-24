33c157729b616170ca3e73b2f5330873b08b6fad
Nguyen Quang Tu <quangtupct@gmail.com>
2023-02-24 15:09:20 +0700

    chore: moved clean script

--- cmd.stop.all.sh
+++ cmd.stop.all.sh
@@ -1,5 +1,2 @@
 echo "Stopping all containers..."
 docker compose --profile backend --profile backend-cuda down
-echo "Cleaning up resources..."
-docker system prune --volumes -a -f
-docker volume rm $(docker volume ls -qf dangling=true)
